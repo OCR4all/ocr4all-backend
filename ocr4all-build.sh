c42e6e91495a474de91e1cb833ddb68f42c0c731
Herbert Baier <baier@itbaier.de>
2024-05-02 14:05:54 +0200

    build/extended for ocr4all or ocrd

--- ocr4all-build.sh
+++ ocr4all-build.sh
@@ -18,25 +18,21 @@ CURRENT_DIR="`pwd`"
 
 MAVEN="mvn"
 
-if ! command -v $MAVEN &> /dev/null
-then
-    echo "The Maven command $MAVEN could not be found"
-    exit 1
-fi
-
-
+#
+# Define variables
+#
 EXIT_STATUS=0
 
 #
-# Initializes build. Default action is help.
+# Font
 #
+RESET='\033[0m'
 
-if [ -n "$1" ]
-then 
-    action="$1"
-else	
-    action="help"
-fi
+BOLD='\033[1m'
+
+# ANSI escape codes for the colored output
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
 
 #
 # Writes a log entry.
@@ -50,6 +46,78 @@ function logEntry()
 }
 
 #
+# Writes a error log entry and aborts the script, this means, exists with status 1.
+#
+# parameter:
+#            $1 The log entry
+#
+function logError()
+{
+	echo -e "${RED}`/bin/date '+%Y-%m-%d %T'` (${BOLD}ERROR${RESET}${RED}): $1${RESET}"
+	cd $CURRENT_DIR
+	
+	exit 1
+}
+
+#
+# Writes a warning log entry and add it to the warning message. The exit status is set to 2.
+#
+# parameter:
+#            $1 The log entry
+#
+WARN_MESSAGES=""
+WARN_INDEX=0
+
+function logWarn()
+{
+	echo -e "${YELLOW}`/bin/date '+%Y-%m-%d %T'` (${BOLD}WARN${RESET}${YELLOW}): $1${RESET}"
+	
+	if [ ! -z "${WARN_MESSAGES}" ]
+	then
+		WARN_MESSAGES="$WARN_MESSAGES\n"
+	fi
+	
+	WARN_MESSAGES="$WARN_MESSAGES\t$((++WARN_INDEX)). $1"
+	
+	EXIT_STATUS=2
+}
+
+#
+# Test if maven command is available
+#
+if ! command -v $MAVEN &> /dev/null
+then
+    logError "the Maven command '$MAVEN' could not be found!"
+fi
+
+#
+# Execute script in the directory where the script is located
+#
+cd "$(dirname "$0")"
+
+if [ $? -ne 0 ]
+then
+	logError "can not change to home directory '$HOME_DIR'!"
+else
+	HOME_DIR="`pwd`"
+fi
+
+#
+# Echo a new line, if not first action
+#
+IS_FIRST_ACTION=true
+
+function echoNewLineAction()
+{
+	if [ "$IS_FIRST_ACTION" = true ]
+	then
+		IS_FIRST_ACTION=false
+	else
+		echo
+	fi
+}
+
+#
 #
 # Install the artifact.
 #
@@ -58,27 +126,22 @@ function logEntry()
 #
 function install_artifact()
 {
-    cd $1
+	echoNewLineAction
+	
+    cd "$HOME_DIR/$1"
     if [ $? -ne 0 ]
     then
-		logEntry "FAIL - can not install the artifact $1 (unknown directory)."
-		
-		EXIT_STATUS=2
+		logWarn "can not install the artifact '$1' (unknown directory $HOME_DIR/$1)."
     else
-    	logEntry "Install artifact: $1"
+    	logEntry "install the artifact '$1'."
     	
 		$MAVEN clean install
 		if [ $? -ne 0 ]
 		then
-	    	logEntry "FAIL - can not install the artifact $1."
-	    	
-	    	EXIT_STATUS=2
+	    	logWarn "can not install the artifact '$1' (maven trouble)."
 		else 
-	    	logEntry "Artifact $1: installed."
-	    	echo
+	    	logEntry "installed the artifact '$1'."
 		fi
-     
-    	cd $CURRENT_DIR
     fi
 }
 
@@ -91,95 +154,194 @@ function install_artifact()
 #
 function package_application()
 {
-    cd $1
+ 	echoNewLineAction
+	
+    cd "$HOME_DIR/$1"
     if [ $? -ne 0 ]
     then
-		logEntry "FAIL - can not package the application $1 (unknown directory)."
-		
-		EXIT_STATUS=2
+		logWarn "can not package the application '$1' (unknown directory $HOME_DIR/$1)."
     else
-    	logEntry "Package application: $1"
+    	logEntry "package the application '$1'."
     	
 		$MAVEN clean package
 		if [ $? -ne 0 ]
 		then
-	    	logEntry "FAIL - can not package the application $1."
-	    	
-	    	EXIT_STATUS=2
+	    	logWarn "can not package the application '$1' (maven trouble)."
 		else 
-	    	logEntry "Application $1: packaged."
-	    	echo
+	    	logEntry "packaged the application '$1'."
 		fi
-     
-    	cd $CURRENT_DIR
     fi
 }
 
 #
-# Install the artifacts.
+# Install ocr4all artifacts.
 #
-function install()
+function install_ocr4all()
 {
-    install_artifact "ocr4all-app-communication"
-    install_artifact "ocr4all-app-spi"
-    install_artifact "ocr4all-app-persistence"
-    install_artifact "ocr4all-app-msa"
-    install_artifact "ocr4all-app-ocrd-communication"
-    install_artifact "ocr4all-app-ocrd-spi"
+	install_artifact "ocr4all-app-communication"
+	install_artifact "ocr4all-app-spi"
+	install_artifact "ocr4all-app-persistence"
+	install_artifact "ocr4all-app-ocrd-communication"
+	install_artifact "ocr4all-app-ocrd-spi"
 }
 
 #
-# Package the applications.
+# Install ocrd artifacts.
 #
-function package()
+function install_ocrd()
+{
+	install_artifact "ocr4all-app-communication"
+	install_artifact "ocr4all-app-msa"
+	install_artifact "ocr4all-app-spi"
+	install_artifact "ocr4all-app-ocrd-communication"
+}
+
+#
+# Install all artifacts.
+#
+function install_all()
+{
+	install_ocr4all
+
+	install_artifact "ocr4all-app-msa"
+}
+
+#
+# Package ocr4all application.
+#
+function package_ocr4all()
 {
     package_application "ocr4all-app"
-    package_application "ocr4all-app-ocrd-msa"
 }
 
 #
-# The main part 
+# Package ocrd applications.
 #
-case "$action" in
-    install)
-		install
-		;;
-		
-     package)
-		package
-		;;
-		
-    build)
-		install
-		package
-		;;
-						
-    *)
+function package_ocrd()
+{
+     package_application "ocr4all-app-ocrd-msa"
+}
+
+#
+# Package all applications.
+#
+function package_all()
+{
+    package_ocr4all
+    package_ocrd
+}
+
+#
+# Prints the help message.
+#
+function print_help()
+{
 	cat >&2 <<-EOF 
 	Usage: $0 <command>
 
 	where <command> is one of:
-	   install - installs the artifacts
-	   package - packages the applications
-	        
-	   build   - builds ocr4all, i.e. installs the artifacts and packages the applications
+	   install         - installs all artifacts
+	   package         - packages all applications
+	   build           - builds the complete application, i.e. installs all artifacts and packages all applications
 
-	   help    - this screen
+	   install-ocr4all - installs the ocr4all artifacts
+	   package-ocr4all - packages the ocr4all application
+	   build-ocr4all   - builds the ocr4all application, i.e. installs the ocr4all artifacts and packages the ocr4all application
+
+	   install-ocrd    - installs the ocrd artifacts
+	   package-ocrd    - packages the ocrd application
+	   build-ocrd      - builds the ocrd application, i.e. installs the ocrd artifacts and packages the ocrd application
+
+	   help            - this screen
 	EOF
-	exit 0
-	;;
-esac
+}
 
 #
-# End
+# Initializes build. Default action is help.
+#
+if [ -n "$1" ]
+then 
+    action="$1"
+else	
+    action="help"
+fi
+
+#
+# The main part 
+#
+SECONDS=0
+case "$action" in
+    install)
+		install_all
+		;;
+		
+    package)
+		package_all
+		;;
+		
+    build)
+		install_all
+		package_all
+		;;
+								
+    install-ocr4all)
+		install_ocr4all
+		;;
+		
+    package-ocr4all)
+		package_ocr4all
+		;;
+		
+    build-ocr4all)
+		install_ocr4all
+		package_ocr4all
+		;;
+								
+    install-ocrd)
+		install_ocrd
+		;;
+		
+    package-ocrd)
+		package_ocrd
+		;;
+		
+    build-ocrd)
+		install_ocrd
+		package_ocrd
+		;;
+								
+    help)
+		print_help
+	
+		cd $CURRENT_DIR
+		exit 0
+		;;
+						
+    *)
+		echo -e "${RED}${BOLD}Unknown command '$action'!${RESET}${RED}"
+	
+		print_help
+	
+		echo -n -e "${RESET}"
+		
+		cd $CURRENT_DIR
+		exit 1
+		;;
+esac
+DURATION=$SECONDS
+
+#
+# Ends the application
 #
 
 if [ "$EXIT_STATUS" -eq "0" ]
 then
-	logEntry "Build ends successfully."
+	logEntry "action '$action' ends successfully."
 else
-	logEntry "Build ends with errors."
+	echo -e "${YELLOW}`/bin/date '+%Y-%m-%d %T'` (${BOLD}WARN${RESET}${YELLOW}): action '$action' ends with following warnings:\n$WARN_MESSAGES${RESET}"
 fi
 
+echo "Running time: $((DURATION / 60)) minutes and $((DURATION % 60)) seconds."
 
+cd $CURRENT_DIR
 exit $EXIT_STATUS
